a04447443a1d97c19753ca912d8c95400fe467e3
Pallavi-0210 <pallavitejaswi596@gmail.com>
2025-06-13 01:29:33 +0530

    authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 function authenticateJWT(req, res, next) {
     const authHeader = req.headers.authorization;
